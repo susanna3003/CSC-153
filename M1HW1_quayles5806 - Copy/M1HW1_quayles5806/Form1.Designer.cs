98781e5b98974a510bae50da0e65bf543f5bca50
susanna3003 <113452531+susanna3003@users.noreply.github.com>
2023-01-23 09:30:36 -0500

    Name Formatter

---
+++ M1HW1_quayles5806 - Copy/M1HW1_quayles5806/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace M1HW1_quayles5806
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prefTitle = new System.Windows.Forms.Label();
+            this.fName = new System.Windows.Forms.Label();
+            this.mName = new System.Windows.Forms.Label();
+            this.lasName = new System.Windows.Forms.Label();
+            this.prefTitleText = new System.Windows.Forms.TextBox();
+            this.fNameText = new System.Windows.Forms.TextBox();
+            this.mNameText = new System.Windows.Forms.TextBox();
+            this.lasNameText = new System.Windows.Forms.TextBox();
+            this.nameForm1 = new System.Windows.Forms.Button();
+            this.nameForm2 = new System.Windows.Forms.Button();
+            this.nameForm3 = new System.Windows.Forms.Button();
+            this.nameForm4 = new System.Windows.Forms.Button();
+            this.nameForm5 = new System.Windows.Forms.Button();
+            this.nameForm6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prefTitle
+            // 
+            this.prefTitle.AutoSize = true;
+            this.prefTitle.Location = new System.Drawing.Point(33, 54);
+            this.prefTitle.Name = "prefTitle";
+            this.prefTitle.Size = new System.Drawing.Size(73, 13);
+            this.prefTitle.TabIndex = 0;
+            this.prefTitle.Text = "Preferred Title";
+            this.prefTitle.Click += new System.EventHandler(this.prefTitle_Click);
+            // 
+            // fName
+            // 
+            this.fName.AutoSize = true;
+            this.fName.Location = new System.Drawing.Point(49, 95);
+            this.fName.Name = "fName";
+            this.fName.Size = new System.Drawing.Size(57, 13);
+            this.fName.TabIndex = 1;
+            this.fName.Text = "First Name";
+            this.fName.Click += new System.EventHandler(this.fName_Click);
+            // 
+            // mName
+            // 
+            this.mName.AutoSize = true;
+            this.mName.Location = new System.Drawing.Point(37, 139);
+            this.mName.Name = "mName";
+            this.mName.Size = new System.Drawing.Size(69, 13);
+            this.mName.TabIndex = 2;
+            this.mName.Text = "Middle Name";
+            this.mName.Click += new System.EventHandler(this.mName_Click);
+            // 
+            // lasName
+            // 
+            this.lasName.AutoSize = true;
+            this.lasName.Location = new System.Drawing.Point(48, 186);
+            this.lasName.Name = "lasName";
+            this.lasName.Size = new System.Drawing.Size(58, 13);
+            this.lasName.TabIndex = 3;
+            this.lasName.Text = "Last Name";
+            this.lasName.Click += new System.EventHandler(this.lasName_Click);
+            // 
+            // prefTitleText
+            // 
+            this.prefTitleText.Location = new System.Drawing.Point(130, 51);
+            this.prefTitleText.Name = "prefTitleText";
+            this.prefTitleText.Size = new System.Drawing.Size(100, 20);
+            this.prefTitleText.TabIndex = 4;
+            this.prefTitleText.TextChanged += new System.EventHandler(this.prefTitleText_TextChanged);
+            // 
+            // fNameText
+            // 
+            this.fNameText.Location = new System.Drawing.Point(130, 92);
+            this.fNameText.Name = "fNameText";
+            this.fNameText.Size = new System.Drawing.Size(100, 20);
+            this.fNameText.TabIndex = 5;
+            this.fNameText.TextChanged += new System.EventHandler(this.fNameText_TextChanged);
+            // 
+            // mNameText
+            // 
+            this.mNameText.Location = new System.Drawing.Point(130, 136);
+            this.mNameText.Name = "mNameText";
+            this.mNameText.Size = new System.Drawing.Size(100, 20);
+            this.mNameText.TabIndex = 6;
+            this.mNameText.TextChanged += new System.EventHandler(this.mNameText_TextChanged);
+            // 
+            // lasNameText
+            // 
+            this.lasNameText.Location = new System.Drawing.Point(130, 183);
+            this.lasNameText.Name = "lasNameText";
+            this.lasNameText.Size = new System.Drawing.Size(100, 20);
+            this.lasNameText.TabIndex = 7;
+            this.lasNameText.TextChanged += new System.EventHandler(this.lasNameText_TextChanged);
+            // 
+            // nameForm1
+            // 
+            this.nameForm1.Location = new System.Drawing.Point(40, 267);
+            this.nameForm1.Name = "nameForm1";
+            this.nameForm1.Size = new System.Drawing.Size(75, 46);
+            this.nameForm1.TabIndex = 8;
+            this.nameForm1.Text = "Form 1";
+            this.nameForm1.UseVisualStyleBackColor = true;
+            this.nameForm1.Click += new System.EventHandler(this.nameForm1_Click);
+            // 
+            // nameForm2
+            // 
+            this.nameForm2.Location = new System.Drawing.Point(155, 267);
+            this.nameForm2.Name = "nameForm2";
+            this.nameForm2.Size = new System.Drawing.Size(75, 46);
+            this.nameForm2.TabIndex = 9;
+            this.nameForm2.Text = "Form 2";
+            this.nameForm2.UseVisualStyleBackColor = true;
+            this.nameForm2.Click += new System.EventHandler(this.nameForm2_Click);
+            // 
+            // nameForm3
+            // 
+            this.nameForm3.Location = new System.Drawing.Point(264, 267);
+            this.nameForm3.Name = "nameForm3";
+            this.nameForm3.Size = new System.Drawing.Size(75, 46);
+            this.nameForm3.TabIndex = 10;
+            this.nameForm3.Text = "Form 3";
+            this.nameForm3.UseVisualStyleBackColor = true;
+            this.nameForm3.Click += new System.EventHandler(this.nameForm3_Click);
+            // 
+            // nameForm4
+            // 
+            this.nameForm4.Location = new System.Drawing.Point(40, 340);
+            this.nameForm4.Name = "nameForm4";
+            this.nameForm4.Size = new System.Drawing.Size(75, 46);
+            this.nameForm4.TabIndex = 11;
+            this.nameForm4.Text = "Form 4";
+            this.nameForm4.UseVisualStyleBackColor = true;
+            this.nameForm4.Click += new System.EventHandler(this.nameForm4_Click);
+            // 
+            // nameForm5
+            // 
+            this.nameForm5.Location = new System.Drawing.Point(155, 340);
+            this.nameForm5.Name = "nameForm5";
+            this.nameForm5.Size = new System.Drawing.Size(75, 46);
+            this.nameForm5.TabIndex = 12;
+            this.nameForm5.Text = "Form 5";
+            this.nameForm5.UseVisualStyleBackColor = true;
+            this.nameForm5.Click += new System.EventHandler(this.nameForm5_Click);
+            // 
+            // nameForm6
+            // 
+            this.nameForm6.Location = new System.Drawing.Point(264, 340);
+            this.nameForm6.Name = "nameForm6";
+            this.nameForm6.Size = new System.Drawing.Size(75, 46);
+            this.nameForm6.TabIndex = 13;
+            this.nameForm6.Text = "Form 6";
+            this.nameForm6.UseVisualStyleBackColor = true;
+            this.nameForm6.Click += new System.EventHandler(this.nameForm6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 520);
+            this.Controls.Add(this.nameForm6);
+            this.Controls.Add(this.nameForm5);
+            this.Controls.Add(this.nameForm4);
+            this.Controls.Add(this.nameForm3);
+            this.Controls.Add(this.nameForm2);
+            this.Controls.Add(this.nameForm1);
+            this.Controls.Add(this.lasNameText);
+            this.Controls.Add(this.mNameText);
+            this.Controls.Add(this.fNameText);
+            this.Controls.Add(this.prefTitleText);
+            this.Controls.Add(this.lasName);
+            this.Controls.Add(this.mName);
+            this.Controls.Add(this.fName);
+            this.Controls.Add(this.prefTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label prefTitle;
+        private System.Windows.Forms.Label fName;
+        private System.Windows.Forms.Label mName;
+        private System.Windows.Forms.Label lasName;
+        private System.Windows.Forms.TextBox prefTitleText;
+        private System.Windows.Forms.TextBox fNameText;
+        private System.Windows.Forms.TextBox mNameText;
+        private System.Windows.Forms.TextBox lasNameText;
+        private System.Windows.Forms.Button nameForm1;
+        private System.Windows.Forms.Button nameForm2;
+        private System.Windows.Forms.Button nameForm3;
+        private System.Windows.Forms.Button nameForm4;
+        private System.Windows.Forms.Button nameForm5;
+        private System.Windows.Forms.Button nameForm6;
+    }
+}
+
---
+++ M1HW1_quayles5806 - Copy/M1HW1_quayles5806/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * 1/20/23
+ * CSC 153
+ * Susanna Quayle
+ * Name Formatter - user inputs preferred title, first, middle, and last name - program provides 6 different formats of user name
+ */
+namespace M1HW1_quayles5806
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void prefTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lasName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prefTitleText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lasNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameForm1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(text: $"{prefTitleText.Text} {fNameText.Text} {mNameText.Text} {lasNameText.Text}");
+        }
+
+        private void nameForm2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(text: $"{fNameText.Text} {mNameText.Text} {lasNameText.Text}");
+        }
+
+        private void nameForm3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(text: $"{fNameText.Text} {lasNameText.Text}");
+        }
+
+        private void nameForm4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(text: $"{lasNameText.Text}, {fNameText.Text} {mNameText.Text}, {prefTitleText.Text}");
+        }
+
+        private void nameForm5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(text: $"{lasNameText.Text}, {fNameText.Text} {mNameText.Text}");
+        }
+
+        private void nameForm6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(text: $"{lasNameText.Text}, {fNameText.Text}");
+        }
+    }
+}

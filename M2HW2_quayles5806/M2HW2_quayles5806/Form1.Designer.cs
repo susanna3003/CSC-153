bfeb7dbf52208fb3ff5b631db7a30c98ba429961
susu <113452531+susanna3003@users.noreply.github.com>
2023-03-13 10:19:50 -0400

    M2HW2 - Change for $ game

---
+++ M2HW2_quayles5806/M2HW2_quayles5806/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace M2HW2_quayles5806
+{
+    partial class dollarGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.subheadLabel = new System.Windows.Forms.Label();
+            this.dollarLabel = new System.Windows.Forms.Label();
+            this.pennyLabel = new System.Windows.Forms.Label();
+            this.nickelLabel = new System.Windows.Forms.Label();
+            this.dimeLabel = new System.Windows.Forms.Label();
+            this.quarterLabel = new System.Windows.Forms.Label();
+            this.pennyTextBox = new System.Windows.Forms.TextBox();
+            this.nickelTextBox = new System.Windows.Forms.TextBox();
+            this.dimeTextBox = new System.Windows.Forms.TextBox();
+            this.quarterTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.Location = new System.Drawing.Point(208, 9);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(307, 31);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "Change For a Dollar Game";
+            // 
+            // subheadLabel
+            // 
+            this.subheadLabel.AutoSize = true;
+            this.subheadLabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subheadLabel.Location = new System.Drawing.Point(260, 55);
+            this.subheadLabel.Name = "subheadLabel";
+            this.subheadLabel.Size = new System.Drawing.Size(193, 21);
+            this.subheadLabel.TabIndex = 1;
+            this.subheadLabel.Text = "Enter change for a dollar";
+            // 
+            // dollarLabel
+            // 
+            this.dollarLabel.AutoSize = true;
+            this.dollarLabel.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dollarLabel.Location = new System.Drawing.Point(539, 116);
+            this.dollarLabel.Name = "dollarLabel";
+            this.dollarLabel.Size = new System.Drawing.Size(143, 109);
+            this.dollarLabel.TabIndex = 2;
+            this.dollarLabel.Text = "$1";
+            // 
+            // pennyLabel
+            // 
+            this.pennyLabel.AutoSize = true;
+            this.pennyLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pennyLabel.Location = new System.Drawing.Point(52, 116);
+            this.pennyLabel.Name = "pennyLabel";
+            this.pennyLabel.Size = new System.Drawing.Size(58, 19);
+            this.pennyLabel.TabIndex = 3;
+            this.pennyLabel.Text = "Pennies:";
+            // 
+            // nickelLabel
+            // 
+            this.nickelLabel.AutoSize = true;
+            this.nickelLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nickelLabel.Location = new System.Drawing.Point(52, 162);
+            this.nickelLabel.Name = "nickelLabel";
+            this.nickelLabel.Size = new System.Drawing.Size(58, 19);
+            this.nickelLabel.TabIndex = 4;
+            this.nickelLabel.Text = "Nickels:";
+            // 
+            // dimeLabel
+            // 
+            this.dimeLabel.AutoSize = true;
+            this.dimeLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dimeLabel.Location = new System.Drawing.Point(60, 206);
+            this.dimeLabel.Name = "dimeLabel";
+            this.dimeLabel.Size = new System.Drawing.Size(50, 19);
+            this.dimeLabel.TabIndex = 5;
+            this.dimeLabel.Text = "Dimes:";
+            // 
+            // quarterLabel
+            // 
+            this.quarterLabel.AutoSize = true;
+            this.quarterLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quarterLabel.Location = new System.Drawing.Point(45, 247);
+            this.quarterLabel.Name = "quarterLabel";
+            this.quarterLabel.Size = new System.Drawing.Size(65, 19);
+            this.quarterLabel.TabIndex = 6;
+            this.quarterLabel.Text = "Quarters:";
+            // 
+            // pennyTextBox
+            // 
+            this.pennyTextBox.Location = new System.Drawing.Point(116, 115);
+            this.pennyTextBox.Name = "pennyTextBox";
+            this.pennyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pennyTextBox.TabIndex = 7;
+            // 
+            // nickelTextBox
+            // 
+            this.nickelTextBox.Location = new System.Drawing.Point(116, 161);
+            this.nickelTextBox.Name = "nickelTextBox";
+            this.nickelTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nickelTextBox.TabIndex = 8;
+            // 
+            // dimeTextBox
+            // 
+            this.dimeTextBox.Location = new System.Drawing.Point(116, 205);
+            this.dimeTextBox.Name = "dimeTextBox";
+            this.dimeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.dimeTextBox.TabIndex = 9;
+            // 
+            // quarterTextBox
+            // 
+            this.quarterTextBox.Location = new System.Drawing.Point(116, 246);
+            this.quarterTextBox.Name = "quarterTextBox";
+            this.quarterTextBox.Size = new System.Drawing.Size(100, 20);
+            this.quarterTextBox.TabIndex = 10;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(126, 309);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 11;
+            this.submitButton.Text = "SUBMIT";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // dollarGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.quarterTextBox);
+            this.Controls.Add(this.dimeTextBox);
+            this.Controls.Add(this.nickelTextBox);
+            this.Controls.Add(this.pennyTextBox);
+            this.Controls.Add(this.quarterLabel);
+            this.Controls.Add(this.dimeLabel);
+            this.Controls.Add(this.nickelLabel);
+            this.Controls.Add(this.pennyLabel);
+            this.Controls.Add(this.dollarLabel);
+            this.Controls.Add(this.subheadLabel);
+            this.Controls.Add(this.headerLabel);
+            this.Name = "dollarGameForm";
+            this.Text = "Change For Dollar Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Label subheadLabel;
+        private System.Windows.Forms.Label dollarLabel;
+        private System.Windows.Forms.Label pennyLabel;
+        private System.Windows.Forms.Label nickelLabel;
+        private System.Windows.Forms.Label dimeLabel;
+        private System.Windows.Forms.Label quarterLabel;
+        private System.Windows.Forms.TextBox pennyTextBox;
+        private System.Windows.Forms.TextBox nickelTextBox;
+        private System.Windows.Forms.TextBox dimeTextBox;
+        private System.Windows.Forms.TextBox quarterTextBox;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
+
---
+++ M2HW2_quayles5806/M2HW2_quayles5806/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/* 
+ * 2/15/2023
+ * CSC 153
+ * Susanna Quayle
+ * Change For A Dollar Game
+ */
+
+namespace M2HW2_quayles5806
+{
+    public partial class dollarGameForm : Form
+    {
+        public dollarGameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            //declare double versions of change input and parse
+            double userPennies, userNickels, userDimes, userQuarters, userAmount;
+            userPennies = double.Parse(pennyTextBox.Text);
+            userNickels = double.Parse(nickelTextBox.Text);
+            userDimes = double.Parse(dimeTextBox.Text);
+            userQuarters = double.Parse(quarterTextBox.Text);
+
+            //get real value of change
+            userPennies = userPennies * 0.01;
+            userNickels = userNickels * 0.05;
+            userDimes = userDimes * 0.1;
+            userQuarters = userQuarters * 0.25;
+
+            userAmount = userPennies + userNickels + userDimes + userQuarters;
+
+            if (userAmount == 1.0)
+            {
+                MessageBox.Show("Congratulations! You have won the game!");
+            }
+            else if (userAmount > 1.0)
+            {
+                MessageBox.Show("Your amount was more than $1. Try again!");
+            }
+            else
+            {
+                MessageBox.Show("Your amount was less than $1. Try again!");
+            }
+
+        }
+    }
+}

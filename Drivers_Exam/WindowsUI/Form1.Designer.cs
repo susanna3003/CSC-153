af4a31cad5c1529a5f122aff8d2573976f386714
susu <113452531+susanna3003@users.noreply.github.com>
2023-04-07 15:42:06 -0400

    Driver's License Exam - Using Arrays
    Need to fix Streamreader problem

---
+++ Drivers_Exam/WindowsUI/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace WindowsUI
+{
+    partial class DriverLicenseExam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gradeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gradeButton
+            // 
+            this.gradeButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gradeButton.Font = new System.Drawing.Font("Myanmar Text", 28.14545F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeButton.Location = new System.Drawing.Point(270, 118);
+            this.gradeButton.Name = "gradeButton";
+            this.gradeButton.Size = new System.Drawing.Size(212, 155);
+            this.gradeButton.TabIndex = 0;
+            this.gradeButton.Text = "GRADE";
+            this.gradeButton.UseVisualStyleBackColor = false;
+            this.gradeButton.Click += new System.EventHandler(this.gradeButton_Click);
+            // 
+            // DriverLicenseExam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gradeButton);
+            this.Name = "DriverLicenseExam";
+            this.Text = "Driver\'s License Exam";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button gradeButton;
+    }
+}
+
---
+++ Drivers_Exam/WindowsUI/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Driver_Exam_Library;
+using System.IO;
+
+/**
+* 4/6/2023
+* CSC 153
+* Susanna Quayle
+* Driver's License Exam
+*/
+
+namespace WindowsUI
+{
+    public partial class DriverLicenseExam : Form
+    {
+        public DriverLicenseExam()
+        {
+            InitializeComponent();
+        }
+
+        private void gradeButton_Click(object sender, EventArgs e)
+        {
+            //store correct exam answers in array
+            String[] examAnswers = { "B", "D", "A", "A", "C", "A", "B", "A", "C", "D", "B", "C", "D", "A", "D", "C", "C", "B", "D", "A" };
+
+            //create studentAnswers.txt
+            //StreamWriter writeStudentAnswers;
+            //writeStudentAnswers = File.CreateText("studentAnswers.txt");
+
+            //read answers from student text into array
+            StreamReader readStudentAns;
+            readStudentAns = File.OpenText("studentAnswers.txt");
+
+            //create studentAnswers array
+            const int SIZE = 20;
+            String[] studentAnswers = new string[SIZE];
+            
+            for (int i = 0; i < SIZE; i++)
+            {
+                while (!readStudentAns.EndOfStream)
+                {
+                    studentAnswers[i] = readStudentAns.ReadLine();
+                }
+            }
+
+            //compare and calculate correct ans
+            int numCorrect = 0;
+            int numWrong = 0;
+            Boolean pass = false;
+
+            for (int j = 0; j < SIZE; j++)
+            {
+                if (examAnswers[j] == studentAnswers[j])
+                {
+                    numCorrect++;
+                }
+                else if (examAnswers[j] != studentAnswers[j])
+                {
+                    numWrong++;
+                }
+            }
+
+            if (numCorrect >= 15)
+            {
+                pass = true;
+            }
+            
+            if (pass == true)
+            {
+                MessageBox.Show("You passed!");
+            }
+            else if (pass == false)
+            {
+                MessageBox.Show("You failed. Better luck next time.");
+            }
+        }
+    }
+}

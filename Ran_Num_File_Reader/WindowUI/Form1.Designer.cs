63bbcb28fc69ed8657f963093a94de40b2970543
susu <113452531+susanna3003@users.noreply.github.com>
2023-05-08 09:37:39 -0400

    M3HW4 - File Reader

---
+++ Ran_Num_File_Reader/WindowUI/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace WindowUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(0, 279);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(800, 173);
+            this.listBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(281, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Find the Random Numbers File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ran_Num_File_Reader/WindowUI/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/**
+* 3/15/2023
+* CSC 153
+* Susanna Quayle
+* Random Number File Reader
+*/
+
+namespace WindowUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create file reading object
+            StreamReader readingFile;
+            int amountNum = 0;
+            int total = 0;
+
+            //read file to list box
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                readingFile = File.OpenText(openFile.FileName);
+                while (!readingFile.EndOfStream)
+                {
+                    int value = int.Parse(readingFile.ReadLine()); 
+                    listBox.Items.Add(value.ToString());
+                    amountNum++;
+                    total += value;
+                }
+                listBox.Items.Add($"Total randoms: {amountNum}");
+                listBox.Items.Add($"Total sum of the randoms: {total}");
+            }
+           
+        }
+    }
+}

9590114001d8aa318a9900734c5839ce68781480
susu <113452531+susanna3003@users.noreply.github.com>
2023-05-08 09:36:17 -0400

    M3HW2 - Random Num Guessing Game

---
+++ Random_Number_Guessing_Game/WindowUI/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace WindowUI
+{
+    partial class randomNumForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.randomNumHeader = new System.Windows.Forms.Label();
+            this.userGuessTextBox = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // randomNumHeader
+            // 
+            this.randomNumHeader.AutoSize = true;
+            this.randomNumHeader.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.randomNumHeader.Location = new System.Drawing.Point(218, 9);
+            this.randomNumHeader.Name = "randomNumHeader";
+            this.randomNumHeader.Size = new System.Drawing.Size(346, 28);
+            this.randomNumHeader.TabIndex = 0;
+            this.randomNumHeader.Text = "Guess a number between 1-100";
+            // 
+            // userGuessTextBox
+            // 
+            this.userGuessTextBox.Location = new System.Drawing.Point(334, 74);
+            this.userGuessTextBox.Name = "userGuessTextBox";
+            this.userGuessTextBox.Size = new System.Drawing.Size(87, 20);
+            this.userGuessTextBox.TabIndex = 1;
+            // 
+            // guessButton
+            // 
+            this.guessButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.guessButton.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.guessButton.Location = new System.Drawing.Point(334, 124);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(87, 50);
+            this.guessButton.TabIndex = 2;
+            this.guessButton.Text = "GUESS";
+            this.guessButton.UseVisualStyleBackColor = false;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(285, 223);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(190, 79);
+            this.resultLabel.TabIndex = 8;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // randomNumForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.userGuessTextBox);
+            this.Controls.Add(this.randomNumHeader);
+            this.Name = "randomNumForm";
+            this.Text = "Random Number Guessing Game";
+            this.Load += new System.EventHandler(this.randomNumForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label randomNumHeader;
+        private System.Windows.Forms.TextBox userGuessTextBox;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Random_Number_Guessing_Game/WindowUI/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+* 3/1/2023
+* CSC 153
+* Susanna Quayle
+* Random Number Guessing Game
+*/
+
+namespace WindowUI
+{
+    public partial class randomNumForm : Form
+    {
+        public randomNumForm()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+
+        int count = 0, randomNum = 0, userGuess = 0;
+        //randomNum = rand.Next(1, 100) + 1;
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            // import random num
+            Random rand = new Random();
+
+            // declare variables
+            //int count = 0, randomNum = 0, userGuess = 0;
+            bool correctGuess = false;
+
+            // create random num
+            //randomNum = rand.Next(1, 100) + 1;
+
+           
+            userGuess = int.Parse(userGuessTextBox.Text);
+            if (userGuess > randomNum)
+            {
+                resultLabel.Text = ("Guess is too high, try again!");
+                count++;
+                userGuessTextBox.Clear();
+            }
+            else if (userGuess < randomNum)
+            {
+                resultLabel.Text = ("Guess is too low, try again!");
+                count++;
+                userGuessTextBox.Clear();
+            }
+            else
+            {
+                resultLabel.Text = ("You got it! And it only took you " + count + " tries!");
+                correctGuess = true;
+            }
+
+
+        }
+
+        private void randomNumForm_Load(object sender, EventArgs e)
+        {
+            randomNum = rand.Next(1, 100) + 1;
+        }
+    }
+}
